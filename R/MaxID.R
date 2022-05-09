060c93c246f201387a27becfcb82a1225a53729b
JianqiaoWang <lukewangjq@gmail.com>
2022-05-09 14:11:35 -0400

    updates on the main algorithm

--- R/MaxID.R
+++ R/MaxID.R
@@ -15,43 +15,81 @@
 #'
 #'
 
-MinID = function(P1, P2,type ="FWER", alpha = 0.05, eff.ratio = 0.25,
+#Pr_M = function(P.min, M,
+#                ref.geno = NULL,
+#                ref.bed = NULL,
+#                block.map = NULL,
+#                var.name = NULL,
+#                output.dir = "./temp/",
+#                LD.thresh,
+#                K = 1000)
+Pr_M = function(M){
+
+  F_min_M = mean(P.min <= M)
+  I.m = which(P.min <= M)
+  I.m.size = length(I.m)
+  P.min.target =  P.min[I.m]
+  if(is.null(var.name)){
+    var.name.target = I.m
+  }else{
+    var.name.target =  var.name[I.m]
+  }
+  names(P.min.target) = var.name.target
+  pr.dense = rep(pr.dense.ind(M, F_min_M), I.m.size)
+
+  if(is.null(ref.geno) & is.null(ref.bed) ){
+    return(prod(pr.dense))
+  }
+  #------------------ read the genotype file ------------------------
+  if(!is.null(ref.bed)){
+    ref.geno = snpStats::read.plink( bed = ref.bed,
+                                     select.snps = var.name.target )
+    ref.geno = as(ref.geno$genotypes, "numeric")
+  }
+  #--------------------- define the block list -------------------
+  if(is.null(block.map)){
+    block.list = Find.plink.block(snplist = var.name.target,
+                                  ref.bed = ref.bed,
+                                  output.dir = output.dir)
+  }else{
+    block.list = split(var.name.target, block.map[I.m])
+  }
+  n.block = length(block.list)
+  n.eff = n.eff.vec(ref.geno, block.list)
+  return(( prod(pr.dense) )^(n.eff/I.m.size))
+}
+
+
+MinID = function(P1, P2,type ="FWER", alpha = 0.05,
                  ref.geno = NULL,
                  ref.bed = NULL,
                  block.map = NULL,
                  var.name = NULL,
                  output.dir = "./temp/",
-                 simple = T,
+                 LD.thresh,
                  K = 1000){
 
+    #-------------------------------------------
+    # If the criteria is to control the FWER
+    #--------------------------------------------
     P.min = pmin(P1, P2)
     P.max = pmax(P1, P2)
     M = min(P.max)
+    M.choice = sort(unique(c(P.max) ), decreasing = FALSE)[1:500]
 
-    if(type == "FWER"){
-      #-------------------------------------------
-      # If the criteria is to control the FWER
-      #--------------------------------------------
-      if(Pv_M_prod(M, P.min, eff.ratio) > alpha ){return(list(S = NULL, tau = NULL))}else{
+    if(type == "FWER"){  # minimum p value
 
-        tau = Search.simple(P.min, P.max, eff.ratio, alpha = alpha)
+      if((1 - Pr_M(M.choice[1])) > alpha ){return(list(S = NULL, tau = NULL))}else{
 
-        if(!simple){ # one more round of updating eff ratio
-        Pv.tau = Pv_given_M(M = tau, P.min = P.min, ref.geno = ref.geno, ref.bed = ref.bed,
-             block.map = block.map, var.name = var.name, output.dir = output.dir )
+        pr.m.choice = (sapply(M.choice, Pr_M_prod, P.min = P.min))^K
 
-        print(Pv.tau)
+        tau = max( M.choice[ (1 - pr.m.choice) <= alpha])
 
-        eff.ratio = Pv.tau$n.eff/Pv.tau$I.m.size  # update the eff.ratio
-
-        print(eff.ratio)
-
-        tau = Search.simple(P.min = P.min, P.max = P.max, eff.ratio = eff.ratio, alpha = alpha)
-        }
         S =  which(P.max <= tau)
-        }
-      return(list(S = S, snp.name = var.name[S], tau = tau, eff.raio = eff.ratio))
+
+      return(list(S = S, tau = tau))
       }
+    }
 
     #------------------------------
     # If the criteria is FDR
@@ -74,101 +112,39 @@ MinID = function(P1, P2,type ="FWER", alpha = 0.05, eff.ratio = 0.25,
 
 
 
-log.pr.dense.fun = function(M, F_min_M){
-  log.pr.dense = log(1 - M)/F_min_M + ((1- F_min_M)/F_min_M) * (M - M^2/2)/(1 - M)^2
-  return(log.pr.dense)
-}
-
-
-Pv_M_prod = function( M, P.min, eff.ratio, all.len = length(P.min) ){
-  F_min_M = sum(P.min <= M)/all.len
-  I.m.size = sum(P.min <= M)
-  log.pr.dense = log(1 - M)/F_min_M + ((1- F_min_M)/F_min_M) * (M - M^2/2)/(1 - M)^2
-  Pv = 1 - exp( I.m.size * eff.ratio *  log.pr.dense )
-  return(Pv)
-}
-
-
-Search.simple = function(P.min, P.max, eff.ratio, type = "FWER", alpha = 0.05){
-  # search with fixed eff.ratio
-  # return tau, the max cut-off value satisfying the < alpha
-  M = min(P.max)
-  if(type == "FWER"){
-      #--------- decide the the range of M choice ---------
-      all.len = length(P.min)
-      M.choice.0 = sort(exp( log(M) * c(1:10) * 0.1 ))
-      for(k in 2:length(M.choice.0)){
-        if(Pv_M_prod(M.choice.0[k], P.min, eff.ratio) > alpha) break
-      }
-
-      M.lower = M.choice.0[k]
-      M.upper = max(P.max[P.max <= M.choice.0[k-1]] )
-      M.choice = unique(P.max[P.max < M.lower])
-      M.choice = sort(M.choice[M.choice >= M.upper ])
-      P.min = P.min[P.min < max(M.choice) ]
-      pv = sapply(M.choice, Pv_M_prod, P.min = P.min, eff.ratio = eff.ratio, all.len = all.len)
-      print(pv)
-      tau = max( M.choice[pv <= alpha])
-      return(tau)
-  }
-}
-
-
-Pv_given_M = function(M, P.min,
-                ref.geno = NULL,
-                ref.bed = NULL,
-                block.map = NULL,
-                var.name = NULL,
-                output.dir = "./temp/",
-                block.thresh = 0.995,
-                K = 1000,
-                exact = F){
-  # the input is p-value sequence
-  # geno is a bigsnpr object without missing values
-  Tstat = M
+Pr_M_prod = function( M, P.min){
   F_min_M = mean(P.min <= M)
   I.m = which(P.min <= M)
   I.m.size = length(I.m)
   P.min.target =  P.min[I.m]
-  if(is.null(var.name)){
-    var.name.target = I.m
-  }else{
-    var.name.target =  var.name[I.m]
-  }
-  names(P.min.target) = var.name.target
-  pr.sparse = pr.sparse.ind(M, P.min.target)
-  log.pr.dense = log(1 - M)/F_min_M + ((1- F_min_M)/F_min_M) * (M - M^2/2)/(1 - M)^2
+  pr.dense = rep(pr.dense.ind(M, F_min_M), I.m.size)
+  return(prod(pr.dense))
+}
 
-  if(is.null(ref.geno) & is.null(ref.bed) ){
-    cat("Ignoring the dependence")
-    return(list(M = M, pv.sparse = 1 - prod(pr.sparse),
-                pv.dense = 1 - exp(I.m.size * log.pr.dense) ))
-  }
-  #------------------ read the genotype file ------------------------
-  if(!is.null(ref.geno)){
-    loc.target = match(var.name.target, ref.geno$map$marker.ID)
-    ref.geno = ref.geno$genotypes[,loc.target] %>% as.matrix
-    colnames(ref.geno) = var.name.target
-  }else{
 
-    if(!is.null(ref.bed)){
-      ref.geno = snpStats::read.plink( bed = ref.bed,
-                                       select.snps = var.name.target )
-      ref.geno = as(ref.geno$genotypes, "numeric")
+Pv_M_prod = function( M, P.min, Kappa){
+  Pv = 1 - Pr_M_prod( M, P.min)^(Kappa)
+  return(Pv)
+}
+
+MinID.simple = function(P1, P2, Kappa, type = "FWER", alpha = 0.05){
+  P.min = pmin(P1, P2)
+  P.max = pmax(P1, P2)
+  M = min(P.max)
+  M.choice = sort(unique(c(P.max) ), decreasing = FALSE)[1:500]
+  if(type == "FWER"){  # minimum p value
+
+    if( Pv_M_prod(M.choice[1], P.min, Kappa)  > alpha ){return(list(S = NULL, tau = NULL))}else{
+
+      pv = sapply(M.choice, Pv_M_prod, P.min = P.min, Kappa = Kappa)
+
+      tau = max( M.choice[pv <= alpha])
+
+      S =  which(P.max <= tau)
+
+      return(list(S = S, tau = tau))
     }
   }
-  #--------------------- define the block list -------------------
-  if(is.null(block.map)){
-    block.list = Find.plink.block(snplist = var.name.target,
-                                  ref.bed = ref.bed,
-                                  output.dir = output.dir)
-  }else{
-    block.list = split(var.name.target, block.map[I.m])
-  }
-  n.block = length(block.list)
-  n.eff = n.eff.vec(ref.geno, block.list, block.thresh = block.thresh)[2]
-  p.value = 1 - exp(n.eff * log.pr.dense)
-  return(list(M = Tstat, p = p.value, pr.dense = exp(I.m.size * log.pr.dense), I.m.size = (I.m.size),
-              n.block = n.block, n.eff = n.eff))
 }
+
 
--- R/MinMaxP.R
+++ R/MinMaxP.R
@@ -210,6 +210,47 @@ RemoveDuplicate = function(index.arr){
 }
 
 
+MinMaxP.simu = function(P1, P2, var.name = NULL,
+                        block.map = NULL,
+                        corr = NULL){
+
+  # the input is p-value sequence
+  P.min = pmin(P1, P2)
+  P.max = pmax(P1, P2)
+  M = min(P.max)
+  Tstat = M
+  F_min_M = mean(P.min <= M)
+  I.m = which(P.min <= M)
+  I.m.size = length(I.m)
+  P.min.target =  P.min[I.m]
+  if(is.null(var.name)){
+    var.name.target = I.m
+  }else{
+    var.name.target =  var.name[I.m]
+  }
+  names(P.min.target) = var.name.target
+  pr.sparse = pr.sparse.ind(M, P.min.target)
+  pr.dense = rep(pr.dense.ind(M, F_min_M), I.m.size)
+
+  #Adj = Adj.func(P1, P2)
+  #A = -log(Adj)
+
+  if(is.null(block.map) & is.null(corr) ){
+    cat("Ignoring the dependence")
+    return(list(M = M, pv.sparse = 1 - prod(pr.sparse),
+                pv.dense = 1 - prod(pr.dense)))
+  }
+  #------------------ read the genotype file ------------------------
+  block.list = split(var.name.target, block.map[I.m])
+  n.block = length(block.list)
+  #--------------------------------------------------------------
+  n.eff = n.eff.vec.simu(SIGMA = corr, block.list = block.list)
+  p.value = 1 - ( prod(pr.dense) )^(n.eff/I.m.size)
+  return(list(M = Tstat, p = p.value, pr.dense = prod(pr.dense),  I.m.len = length(I.m),
+              n.block = n.block, n.eff = n.eff))
+}
+
+
 n.eff.vec.simu <- function(SIGMA, block.list){
   n.eff = lapply(block.list, function(x){
     if(length(x) == 1){
